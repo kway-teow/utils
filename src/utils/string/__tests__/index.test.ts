0b3d63787879629f64490b4f1b88fcc169897460
cola <icepepsicola@foxmail.com>
2025-03-25 23:09:14 +0800

    fix: 🛠️ 更新 pre-commit 钩子以包含测试运行，并修正测试文件中的导入路径

--- src/utils/string/__tests__/index.test.ts
+++ src/utils/string/__tests__/index.test.ts
@@ -1,5 +1,5 @@
 import { describe, test, expect } from 'vitest'
-import { capitalize, camelToKebab, kebabToCamel } from './index'
+import { capitalize, camelToKebab, kebabToCamel } from '../index'
 
 // 正常输入
 describe('string', () => {
--- src/utils/validation/__tests__/index.test.ts
+++ src/utils/validation/__tests__/index.test.ts
@@ -1,5 +1,5 @@
 import { describe, test, expect } from 'vitest'
-import { isEmail, isChinesePhone, isURL, isChineseIDCard, isEmpty } from './index'
+import { isEmail, isChinesePhone, isURL, isChineseIDCard, isEmpty } from '../index'
 
 describe('isEmail', () => {
   test('should return true for valid email addresses', () => {
